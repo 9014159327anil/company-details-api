c15b5162e502bd3edf34c83935c3f70ccb236632
anil kumar pattem <anilpattem1412@gmail.com>
2022-09-12 14:36:26 +0530

    second project added

---
+++ src/test/java/com/employe/CompanyDetailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyDetailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
